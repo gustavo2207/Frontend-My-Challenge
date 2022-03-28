71f92f9ea698c0428e4ccff28a1c2e8c0de8ee9d
Gustavo Moraes <gustavomercedes1@hotmail.com>
2022-03-28 12:45:01 -0300

    dinamic frontend

--- src/app/button-crud/button-crud.component.ts
+++ src/app/button-crud/button-crud.component.ts
@@ -13,34 +13,43 @@ export class ButtonCrudComponent implements OnInit {
   }
 
   btnCreate(){
-    const isDevice = document.
-    querySelector("#group-btn")?.classList.contains("device");
-
-    if(isDevice){
-      document.querySelector("#create-device")?.classList.replace("hide", "none")
-      document.querySelector("#read-device")?.classList.add("hide", "hide")
-      document.querySelector("#delete-device")?.classList.add("hide", "hide")
+    const isDevice = document.querySelector("#btn-device")?.classList.contains("device");
+    const isCategory = document.querySelector("#btn-category")?.classList.contains("category");
+    if(isDevice) {
+      document.querySelector("#create-device")?.classList.remove("hide")
+      document.querySelector("#read-device")?.classList.add("hide")
+      document.querySelector("#delete-device")?.classList.add("hide")
+    } else if(isCategory){
+      document.querySelector("#create-category")?.classList.remove("hide")
+      document.querySelector("#read-category")?.classList.add("hide")
+      document.querySelector("#delete-category")?.classList.add("hide")
     }
   }
   btnRead(){
-    const isDevice = document.
-    querySelector("#group-btn")?.classList.contains("device");
-
+    const isDevice = document.querySelector("#btn-device")?.classList.contains("device");
+    const isCategory = document.querySelector("#btn-category")?.classList.contains("category");
     if(isDevice){
-      document.querySelector("#create-device")?.classList.add("hide", "hide")
-      document.querySelector("#read-device")?.classList.replace("hide", "none")
-      document.querySelector("#delete-device")?.classList.add("hide", "hide")
+      document.querySelector("#create-device")?.classList.add("hide")
+      document.querySelector("#read-device")?.classList.remove("hide")
+      document.querySelector("#delete-device")?.classList.add("hide")
+    } else if(isCategory){
+      document.querySelector("#create-category")?.classList.add("hide")
+      document.querySelector("#read-category")?.classList.remove("hide")
+      document.querySelector("#delete-category")?.classList.add("hide")
     }
   }
 
   btnDelete(){
-    const isDevice = document.
-    querySelector("#group-btn")?.classList.contains("device");
-
-    if(isDevice){
-      document.querySelector("#create-device")?.classList.add("hide", "hide")
-      document.querySelector("#read-device")?.classList.add("hide", "hide")
-      document.querySelector("#delete-device")?.classList.replace("hide", "none")
+    const isDevice = document.querySelector("#btn-device")?.classList.contains("device");
+    const isCategory = document.querySelector("#btn-category")?.classList.contains("category");
+    if(isDevice) {
+      document.querySelector("#create-device")?.classList.add("hide")
+      document.querySelector("#read-device")?.classList.add("hide")
+      document.querySelector("#delete-device")?.classList.remove("hide")
+    } else if (isCategory) {
+      document.querySelector("#create-category")?.classList.add("hide")
+      document.querySelector("#read-category")?.classList.add("hide")
+      document.querySelector("#delete-category")?.classList.remove("hide")
     }
   }
   
--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -19,6 +19,17 @@ export class CardsComponent implements OnInit {
     event.preventDefault();
     const isShow = document.querySelector("#btn-device")?.classList.contains("hide")
 
+    const isCategory = document
+    .querySelector("#btn-category")?.classList.contains("category")
+
+    if(isCategory) {
+      document.querySelector("#btn-category")?.classList.replace("category", "hide")
+      document.querySelector("#btn-device")?.classList.add("hide")
+      document.querySelector("#create-device")?.classList.add("hide")
+      document.querySelector("#read-device")?.classList.add("hide")
+      document.querySelector("#delete-device")?.classList.add("hide")
+    }
+
     if(isShow){
       document.querySelector("#btn-device")?.classList.remove("hide")
       document.querySelector("#create-device")?.classList.remove("hide")
@@ -29,8 +40,37 @@ export class CardsComponent implements OnInit {
       document.querySelector("#delete-device")?.classList.add("hide")
     }
 
-    document.querySelector("#group-btn")?.classList.add("device")
-    document.querySelector("#group-btn")?.classList.remove("category")
+    document.querySelector("#btn-device")?.classList.toggle("device")
+    document.querySelector("#btn-category")?.classList.remove("category")
+  }
+
+  manageCategory(event: any){
+    event.preventDefault();
+
+    const isShow = document.querySelector("#btn-category")?.classList.contains("hide");
+
+    const isDevice = document
+    .querySelector("#btn-device")?.classList.contains("device")
+
+    if(isDevice) {
+      document.querySelector("#btn-device")?.classList.replace("device", "hide")
+      document.querySelector("#btn-category")?.classList.add("hide")
+      document.querySelector("#create-category")?.classList.add("hide")
+      document.querySelector("#read-category")?.classList.add("hide")
+      document.querySelector("#delete-category")?.classList.add("hide")
+    }
+    if(isShow) {
+      document.querySelector("#btn-category")?.classList.remove("hide")
+      document.querySelector("#create-category")?.classList.remove("hide")
+    }
+    else {
+      document.querySelector("#btn-category")?.classList.add("hide")
+      document.querySelector("#create-category")?.classList.add("hide")
+      document.querySelector("#read-category")?.classList.add("hide")
+      document.querySelector("#delete-category")?.classList.add("hide")
+    }
+    document.querySelector("#btn-category")?.classList.toggle("category")
+    document.querySelector("#btn-device")?.classList.remove("device")
   }
 
 }
